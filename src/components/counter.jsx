bcf8793c65c7301f9fd9275873146aeb87cb9928
unknown <collinstshuma@gmail.com>
2022-08-25 19:11:23 +0200

    feat: Removing the local state

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,31 +1,24 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = {
-    value: this.props.counter.value,
-  };
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
+    const { count } = this.props.counter;
     return count === 0 ? "zero" : count;
   }
-
-  handleIncrement = () => {
-    this.setState({ value: this.state.count + 1 });
-  };
 
   render() {
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={this.handleIncrement}
+          onClick={()=> this.props.onIncrement(this,props.counter)}
           className="btn btn-secondary btn-sm"
         >
           Increment
